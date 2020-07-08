3c59094da731aef0f89a84ddaefe3352431f54a8
Mauro Molin <molin.mauro@femastudios.com>
2020-07-08 11:54:17 +0200

    Added tests

---
+++ tests/files/FilesUtilsTest.php
@@ -0,0 +1,162 @@
+<?php
+    declare(strict_types=1);
+
+    namespace com\femastudios\utils\http\tests\files;
+
+    use com\femastudios\utils\http\files\FilesUtils;
+    use com\femastudios\utils\http\files\UploadedFile;
+    use com\femastudios\utils\http\files\UploadedFileException;
+    use PHPUnit\Framework\TestCase;
+
+    class FilesUtilsTest extends TestCase {
+
+        protected function setUp() : void {
+            $_FILES = [
+                'user' => [
+                    'name'     => [
+                        'info'      => [
+                            'avatar' => 'photo.jpg',
+                        ],
+                        'logo'      => 'logo.png',
+                        'signature' => 'signature.png',
+                    ],
+                    'type'     => [
+                        'info'      => [
+                            'avatar' => 'image/jpeg',
+                        ],
+                        'logo'      => 'image/png',
+                        'signature' => 'image/png',
+                    ],
+                    'tmp_name' => [
+                        'info'      => [
+                            'avatar' => '/tmp/phpA6H4.tmp',
+                        ],
+                        'logo'      => '/tmp/phpL8H4.tmp',
+                        'signature' => '/tmp/phpZ44E.tmp',
+                    ],
+                    'error'    => [
+                        'info'      => [
+                            'avatar' => UPLOAD_ERR_OK,
+                        ],
+                        'logo'      => UPLOAD_ERR_OK,
+                        'signature' => UPLOAD_ERR_FORM_SIZE,
+                    ],
+                    'size'     => [
+                        'info'      => [
+                            'avatar' => 1354716,
+                        ],
+                        'logo'      => 354987,
+                        'signature' => 18596478,
+                    ],
+                ],
+            ];
+        }
+
+        public function testGetReorderedFiles() {
+            self::assertSame([
+                'user' => [
+                    'info'      => [
+                        'avatar' => [
+                            'name'     => 'photo.jpg',
+                            'type'     => 'image/jpeg',
+                            'tmp_name' => '/tmp/phpA6H4.tmp',
+                            'error'    => UPLOAD_ERR_OK,
+                            'size'     => 1354716,
+                        ],
+                    ],
+                    'logo'      => [
+                        'name'     => 'logo.png',
+                        'type'     => 'image/png',
+                        'tmp_name' => '/tmp/phpL8H4.tmp',
+                        'error'    => UPLOAD_ERR_OK,
+                        'size'     => 354987,
+                    ],
+                    'signature' => [
+                        'name'     => 'signature.png',
+                        'type'     => 'image/png',
+                        'tmp_name' => '/tmp/phpZ44E.tmp',
+                        'error'    => UPLOAD_ERR_FORM_SIZE,
+                        'size'     => 18596478,
+                    ],
+                ],
+            ], FilesUtils::getReorderedFiles());
+        }
+
+        private static function checkAvatar($avatar) {
+            self::assertInstanceOf(UploadedFile::class, $avatar);
+            self::assertSame($avatar->getName(), 'photo.jpg');
+            self::assertSame($avatar->getType(), 'image/jpeg');
+            self::assertSame($avatar->getTmpName(), '/tmp/phpA6H4.tmp');
+            self::assertSame($avatar->getSize(), 1354716);
+        }
+
+        private static function checkLogo($logo) {
+            self::assertInstanceOf(UploadedFile::class, $logo);
+            self::assertSame($logo->getName(), 'logo.png',);
+            self::assertSame($logo->getType(), 'image/png');
+            self::assertSame($logo->getTmpName(), '/tmp/phpL8H4.tmp',);
+            self::assertSame($logo->getSize(), 354987);
+        }
+
+        public function testGetUploadedFiles() {
+            $uploadedFiles = FilesUtils::getUploadedFiles();
+            self::assertIsCallable($uploadedFiles['user']['info']['avatar']);
+            self::assertIsCallable($uploadedFiles['user']['logo']);
+            self::assertIsCallable($uploadedFiles['user']['signature']);
+
+            /** @var UploadedFile $avatar */
+            $avatar = $uploadedFiles['user']['info']['avatar']();
+            self::checkAvatar($avatar);
+
+            /** @var UploadedFile $logo */
+            $logo = $uploadedFiles['user']['logo']();
+            self::checkLogo($logo);
+
+            $this->expectException(UploadedFileException::class);
+            $this->expectExceptionCode(UPLOAD_ERR_FORM_SIZE);
+            $uploadedFiles['user']['signature']();
+        }
+
+        /** @throws UploadedFileException */
+        public function testOptUploadedFile() {
+            self::checkAvatar(FilesUtils::optUploadedFile('user', 'info', 'avatar'));
+            self::checkLogo(FilesUtils::optUploadedFile('user', 'logo'));
+
+            $this->expectException(UploadedFileException::class);
+            $this->expectExceptionCode(UPLOAD_ERR_FORM_SIZE);
+            FilesUtils::optUploadedFile('user', 'signature');
+
+            self::assertNull(FilesUtils::optUploadedFile('user', 'hello'));
+            self::assertNull(FilesUtils::optUploadedFile('user', 'signature', 'type'));
+        }
+
+        /** @throws UploadedFileException */
+        public function testGetUploadedFile() {
+            self::checkAvatar(FilesUtils::getUploadedFile('user', 'info', 'avatar'));
+            self::checkLogo(FilesUtils::getUploadedFile('user', 'logo'));
+
+            $this->expectException(UploadedFileException::class);
+            $this->expectExceptionCode(UPLOAD_ERR_FORM_SIZE);
+            FilesUtils::getUploadedFile('user', 'signature');
+        }
+
+        /** @throws UploadedFileException */
+        public function testGetUploadedFileNotFound() {
+            $this->expectException(\LogicException::class);
+            FilesUtils::getUploadedFile('user', 'hello');
+        }
+
+        public function testHasUploadedFile() {
+            self::assertTrue(FilesUtils::hasUploadedFile('user', 'info', 'avatar'));
+            self::assertTrue(FilesUtils::hasUploadedFile('user', 'logo'));
+            self::assertTrue(FilesUtils::hasUploadedFile('user', 'signature'));
+            self::assertFalse(FilesUtils::hasUploadedFile('user', 'hello'));
+            self::assertFalse(FilesUtils::hasUploadedFile('123'));
+            self::assertFalse(FilesUtils::hasUploadedFile('user', 'info', 'avatar', 'name'));
+        }
+
+
+        protected function tearDown() : void {
+            $_FILES = [];
+        }
+    }
---
+++ tests/files/UploadedFileTest.php
@@ -0,0 +1,80 @@
+<?php
+    declare(strict_types=1);
+
+    namespace com\femastudios\utils\http\tests\files;
+
+    use com\femastudios\utils\http\files\UploadedFile;
+    use com\femastudios\utils\http\files\UploadedFileException;
+    use PHPUnit\Framework\TestCase;
+
+    class UploadedFileTest extends TestCase {
+        public function testIsValidFilesArray() {
+            $arr = [
+                'name' => 'hello.jpg',
+                'error' => UPLOAD_ERR_OK,
+                'size' => 1234,
+                'tmp_name' => '/tmp/php123.tmp',
+                'ignored' => 'hello'
+            ];
+            self::assertTrue(UploadedFile::isValidFilesArray($arr));
+
+            // Invalid type
+            $arr['type'] = 1234;
+            self::assertFalse(UploadedFile::isValidFilesArray($arr));
+
+            // OK type
+            $arr['type'] = 'image/jpeg';
+            self::assertTrue(UploadedFile::isValidFilesArray($arr));
+
+            // Invalid name
+            $arr['name'] = 1234;
+            self::assertFalse(UploadedFile::isValidFilesArray($arr));
+            $arr['name'] = 'hello.jpg';
+
+            // Invalid size
+            $arr['size'] = "wrong";
+            self::assertFalse(UploadedFile::isValidFilesArray($arr));
+            $arr['size'] = 1234;
+
+            // Invalid tmp_name
+            $arr['tmp_name'] = 1234;
+            self::assertFalse(UploadedFile::isValidFilesArray($arr));
+            $arr['name'] = '/tmp/php123.tmp';
+        }
+
+        /** @throws UploadedFileException */
+        public function testFromFilesArray() {
+            $uf = UploadedFile::fromFilesArray([
+                'name' => 'hello.jpg',
+                'error' => UPLOAD_ERR_OK,
+                'size' => 1234,
+                'tmp_name' => '/tmp/php123.tmp',
+                'ignored' => 'hello'
+            ]);
+            self::assertSame($uf->getName(), 'hello.jpg');
+            self::assertSame($uf->getSize(), 1234);
+            self::assertSame($uf->getTmpName(), '/tmp/php123.tmp');
+            self::assertNull($uf->getType());
+        }
+
+        /** @throws UploadedFileException */
+        public function testFromFilesArrayWrongArray() {
+            $this->expectException(\DomainException::class);
+            UploadedFile::fromFilesArray([
+                'name' => 132,
+                'size' => 1234
+            ]);
+        }
+
+        public function testFromFilesArrayError() {
+            $this->expectException(UploadedFileException::class);
+            $this->expectExceptionCode(UPLOAD_ERR_CANT_WRITE);
+            UploadedFile::fromFilesArray([
+                'name' => 'hello.jpg',
+                'error' => UPLOAD_ERR_CANT_WRITE,
+                'size' => 1234,
+                'tmp_name' => '/tmp/php123.tmp',
+                'ignored' => 'hello'
+            ]);
+        }
+    }
